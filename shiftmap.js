028834f8fbfed84aaa81f68d6f3541e3f04f5989
Danny Kim <dannykim713@gmail.com>
2016-03-29 20:50:51 -0400

    prototype

--- shiftmap.js
+++ shiftmap.js
@@ -1,17 +1,38 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Groups = new Mongo.Collection('Groups');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+Groups.insert({start: 9, end: 17, admin: 'hello', 
+  days: [
+  {day: "sun", shift: [{startS: 10, endS: 11, names: [], max: "10"}, {startS: 11, endS: 12, names: [], max: "10"}, {startS: 11, endS: 12, names: [], max: "10"}]},
+  {day: "mon", shift: []},
+  {day: "tue", shift: []},
+  {day: "wed", shift: []},
+  {day: "thu", shift: []},
+  {day: "fri", shift: []},
+  {day: "sat", shift: []}]
+});
+
+
+//tempId = Coll.insert({counter: 0 }, {hello: 1}), ;
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+      getDays : function () {
+        return Groups.findOne().days;
     }
+  });
+
+  Template.setDay.helpers({
+      mapShift : function (start, end, names) {
+        height = 100;
+        width = "100";
+        return s = "style=\"height:" + height + "px;width:" + width + "px\"";
+
+      }
+    
   });
 
   Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
     }
   });
 }
@@ -19,5 +40,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
   });
 }
